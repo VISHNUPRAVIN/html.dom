26e3f293c6760a0c02bf4adc9eda1a1df87348f1
VISHNUPRAVIN <109369773+VISHNUPRAVIN@users.noreply.github.com>
2022-09-01 02:23:04 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+ function foo(){
+   
+    var res = document.getElementById("main").value;
+    console.log(res);
+    var res1 = document.getElementById("main1").value;
+    console.log(res1);
+    var res2 = document.getElementById("main2").value;
+    console.log(res2);
+    var res3 = document.getElementById("main3").value;
+    console.log(res3);
+
+ }
+
+
+ 
+ 
+
+
